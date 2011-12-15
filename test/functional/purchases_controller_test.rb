e9f4c7a81222c05512effcdda873f65c121af111
Bastien Vaucher <bastien.vaucher@gmail.com>
2011-12-15 13:26:24 +0100

    Added test using Capybara API directly

--- test/functional/purchases_controller_test.rb
+++ test/functional/purchases_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
+require 'capybara/dsl'
+require 'capybara/rails'
 
 class PurchasesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include Capybara::DSL
+  
+  test "Keeping session data afer acessing remote server" do
+    visit '/'
+    click_button("Log in")
+    click_button("Purchase")
+    assert page.has_content?("Status: true"), "User is not logged in"
+  end
 end
